1dcb5fc185e92fe008f7521ea4f8292c99aa9f98
lgomezlope <149296586+lgomezlope@users.noreply.github.com>
2023-10-30 18:21:21 +0100

    Create MiObjectOutputStream.java

---
+++ MiObjectOutputStream.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class MiObjectOutputStream extends ObjectOutputStream
+{
+    /** Constructor que recibe OutputStream */
+    public MiObjectOutputStream(OutputStream out) throws IOException
+    {
+        super(out);
+    }
+
+    /** Constructor sin parámetros */
+    protected MiObjectOutputStream() throws IOException, SecurityException
+    {
+        super();
+    }
+
+    /** Redefinición del método de escribir la cabecera para que no haga nada. */
+    protected void writeStreamHeader() throws IOException
+    {
+    }
+
+}
+
+
+
+
